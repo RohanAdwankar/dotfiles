36859da2546753a6afe14a92541fa32ab5b01da6
Rohan Adwankar <39285979+RohanAdwankar@users.noreply.github.com>
2026-05-26 19:03:35 -0700

    resolve remaning manual setup steps
    install nvim config
    install rohanrust

--- macos.sh
+++ macos.sh
@@ -1,3 +1,7 @@
 brew bundle install
+mkdir -p "$HOME/.config"
+[ -d "$HOME/.config/nvim/.git" ] || git clone https://github.com/RohanAdwankar/nvim.git "$HOME/.config/nvim"
+[ -d "$HOME/rohanrust/.git" ] || git clone https://github.com/RohanAdwankar/rohanrust.git "$HOME/rohanrust"
+cargo build --release --manifest-path "$HOME/rohanrust/Cargo.toml"
 ln -sf "$PWD/.zshrc" ~/.zshrc
 defaults write com.apple.PowerChime ChimeOnNoHardware -bool true;killall PowerChime
